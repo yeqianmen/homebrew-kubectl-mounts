f40e4bdfaaf67f602c1132d276e1494d3e79bb08
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2025-05-15 14:29:14 +0800

    Update kubectl-mounts.rb version v0.0.5

--- Formula/kubectl-mounts.rb
+++ Formula/kubectl-mounts.rb
@@ -2,17 +2,17 @@
 class KubectlMounts < Formula
   desc "Show Pod Volumes and VolumeMounts in the cluster"
   homepage "https://github.com/yeqianmen/kubectl-mounts"
-  version "v0.0.4"
+  version "v0.0.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/yeqianmen/kubectl-mounts/releases/download/v0.0.4/kubectl-mounts_darwin_amd64.tar.gz"
-      sha256 "74a8c3e5c44ad0ab44f0b3ae001e3bd3a87595fa916b22b785af44835e191774"
+      url "https://github.com/yeqianmen/kubectl-mounts/releases/download/v0.0.5/kubectl-mounts_darwin_amd64.tar.gz"
+      sha256 "b340ffffbbd49371dce5c49b044e81972666a8a372d4e091e453b54eb2fe81e3"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/yeqianmen/kubectl-mounts/releases/download/v0.0.4/kubectl-mounts_darwin_arm64.tar.gz"
-      sha256 "dd6b762aea89c20bb337d7fa0ac8afc61aa3f25d88655ef663036e482aac8d34"
+      url "https://github.com/yeqianmen/kubectl-mounts/releases/download/v0.0.5/kubectl-mounts_darwin_arm64.tar.gz"
+      sha256 "1655b716a5c0bd90aa6afca49bbfcabfeb04eb30b7c43dadfe188f215093a9ce"
     end
   end
 
